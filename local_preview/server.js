6c480af49f0a4cc535544b7443fecd9f881732f5
Richard <xuj_power@qq.com>
2017-11-23 01:02:50 +0800

    add: 本地调试增加无效链接返回404页面

--- local_preview/server.js
+++ local_preview/server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 app.use('/blog', express.static('../_site'));
+
+/*
+* 请求过来优先匹配静态路径，
+* 当请求不存在的路径时，才会走这个route
+*/
+app.get('*', function(req, res){
+	var file404 = path.resolve(__dirname, '../_site/404.html');
+
+	// 重定向到404
+	// res.redirect(301, '/blog/404.html');
+	res.sendFile(file404);
+})
 
 app.listen(4848);
 
