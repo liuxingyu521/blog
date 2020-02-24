d93abf749841a6d947a13c95577aca50f048f922
liuxingyu521 <xuj_power@qq.com>
2020-02-24 17:28:43 +0800

    :art: 升级打包模块

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const rename = require('gulp-rename');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const del = require('del');
+
+/**
+ * 清除之前生成的旧文件
+ * @param {string} delType 删除类型
+ */
+const delTask = delType =>
+  function delTask() {
+    const delCssPaths = ['./css/xuer-blog.css', './css/xuer-blog.min.css'];
+    const delJsPaths = ['./js/xuer-blog.min.js'];
+    const delTypeMap = {
+      css: delCssPaths,
+      js: delJsPaths,
+      all: [].concat(delCssPaths, delJsPaths),
+    };
+
+    return del(delTypeMap[delType]);
+  };
+
+/**
+ * 处理 css 文件
+ */
+const cssTask = function() {
+  return src(`./less/xuer-blog.less`)
+    .pipe(
+      less({
+        paths: [`./less/`],
+      })
+    ) // less 转译
+    .pipe(rename('xuer-blog.css')) // 重命名
+    .pipe(dest('./css/')) // 生成转译后 css 文件
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    ) // 自动补全浏览器兼容属性
+    .pipe(csso()) // css 压缩
+    .pipe(rename('xuer-blog.min.css')) // 重命名
+    .pipe(dest(`./css/`)); // 生成压缩后的 css 文件
+};
+
+/**
+ * 处理 js 文件
+ */
+const jsTask = function() {
+  return src('./js/xuer-blog.js')
+    .pipe(uglify())
+    .pipe(rename('xuer-blog.min.js'))
+    .pipe(dest('./js/'));
+};
+
+exports.css = cssTask;
+exports.del = delTask('all');
+exports.js = jsTask;
+
+exports.watch = function() {
+  watch(
+    './js/xuer-blog.js',
+    {
+      ignoreInitial: false,
+    },
+    series(delTask('js'), jsTask)
+  );
+
+  watch(
+    './less/*',
+    {
+      ignoreInitial: false,
+    },
+    series(delTask('css'), cssTask)
+  );
+};
+
+exports.default = series(delTask('all'), parallel(cssTask, jsTask));
--- js/xuer-blog.js
+++ js/xuer-blog.js
@@ -9,16 +9,6 @@ $(function() {
   $("[data-toggle='tooltip']").tooltip();
 });
 
-// make all images responsive
-/*
- * Unuse by Hux
- * actually only Portfolio-Pages can't use it and only post-img need it.
- * so I modify the _layout/post and CSS to make post-img responsive!
- */
-// $(function() {
-// 	$("img").addClass("img-responsive");
-// });
-
 // responsive tables
 $(document).ready(function() {
   $('table').wrap("<div class='table-responsive'></div>");
